90c7cb625a0c05218836b6be1ffb1d6957b61e71
Adelana107 <adelana787898@gmail.com>
2025-10-08 19:22:34 +0100

    Resolved merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
 import App from "./App";
 import StarRating from "./StarRating";
- import "./index.css"; // Uncomment if you have styling
+import "./index.css"; // Include your styles
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -12,19 +11,6 @@ function Test() {
   return (
     <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
       <h2>Rate This Movie 🎬</h2>
-=======
-import { useState } from "react";
-import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
->>>>>>> f0172afd06df8881e852c06a75284453e911114e
       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
       <p>This movie was rated {movieRating} stars</p>
     </div>
@@ -34,18 +20,15 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    {/* Toggle between App and Test for quick testing */}
+    {/* You can toggle between App and Test for testing */}
     <App />
     {/* <Test /> */}
-=======
-    {/* <App /> */}
-    <StarRating
+
+    {/* Example standalone StarRating components */}
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <Test />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
->>>>>>> f0172afd06df8881e852c06a75284453e911114e
+    /> */}
+    {/* <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
   </React.StrictMode>
 );
