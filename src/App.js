37d4fc567d06fbfc0f9384f99c5afbd0234a1e9a
Adelana107 <adelana787898@gmail.com>
2025-10-14 19:11:17 +0100

    added  Refs to Persist Data Between Renders

--- src/App.js
+++ src/App.js
@@ -194,7 +194,6 @@ function Search({ query, setQuery }) {
   );
 }
 
-
 function NumResults({ movies }) {
   return (
     <p className="num-results">
@@ -253,6 +252,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+  let count = 0;
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current = countRef.current++;
+       
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -296,6 +306,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime?.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
